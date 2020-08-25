d1c9fda0593062d030e924e046c7ea48c5a6c80a
cs3cAgile6 <69994847+cs3cAgile6@users.noreply.github.com>
2020-08-25 15:49:32 -0700

    Add files via upload

---
+++ rex.cpp
@@ -0,0 +1,17 @@
+/*
+ * rex.cpp
+ *
+ *  Created on: Aug 23, 2020
+ *      Author: rexsmacbook
+ */
+#include <iostream>
+#include "rex.h"
+using namespace std;
+
+void rex()
+{
+	cout << "Rex Kim" << endl;
+}
+
+
+
